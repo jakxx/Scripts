d108e7cd56777ec54399af44b3ede9e7f84ab747
jakxx <jakx.ppr@gmail.com>
2012-07-01 23:34:53 -0300

    Update master

--- Nessus-grab.sh
+++ Nessus-grab.sh
@@ -37,7 +37,7 @@ cat reportlistclean.txt | while read line; do
                         echo $previousline
                         echo $today
                         cleanreportid=`echo $reportnumber | sed 's/<name>//g' | sed 's%</name>%%g'`
-                        wget --quiet --post-data "report=$cleanreportid&chapters=vuln_by_plugin&format=html&token=$key" --no-check-certificate https://172.16.88.33/chapter? -O - > test.html
+                        wget --quiet --post-data "report=$cleanreportid&chapters=vuln_by_plugin&format=html&token=$key" --no-check-certificate https://$1/chapter? -O - > test.html
                         sed "s%url=%url=https://$i%g" test.html > test2.html
                         url=`cat test2.html | grep "https" | sed 's%<title>Formatting the report</title><meta http-equiv="refresh" content="5;url=%%g' | sed 's/">//g'`
                         echo "generating report"
