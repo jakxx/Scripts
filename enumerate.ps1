e813a8c2f54f4e436cf3af9b93ca93298acdab41
jakxx <jakx.ppr@gmail.com>
2012-08-24 15:11:00 -0400

    Enumerate.ps1

---
+++ enumerate.ps1
@@ -0,0 +1,10 @@
+
+echo "Fetching IP's.."
+ipconfig | select-string "ipv4"
+$ip= Read-Host 'Enter the IP you want to use'
+$subnet=$ip | foreach {$_.split(".")[-4,-3,-2]-join "."}
+$valid= 1..255 | % {$ip2=$subnet + "." + $_; "$ip2 : $(Test-Connection -count 1 -comp $ip2 -quiet)"} | select-string "true"
+$clean= $valid | foreach-object {$_.tostring()} | foreach-object {$_.split(":")} | select-string "[0-9]."
+$clean2= $clean | foreach-object {$_.tostring()} | foreach-object {$_.trim()}
+$clean2 | foreach-object{ echo "..$_" ; Get-WmiObject -class win32_share -computer $_ } > shares.txt
+
