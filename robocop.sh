a905a850aafe1f8363aacaf189a3cbce6015b2a7
jakxx <jakx.ppr@gmail.com>
2012-10-18 16:54:50 -0400

    added robocop

---
+++ robocop.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+input1=`echo $1 | grep -o "\."`
+
+
+if [ -z "$1" ] || [ "$1" = '-h' ] || [ "$1" = '--help' ] || [ -z "$input1" ]; then
+echo "Usage: ./robocop.sh [www.targetsite.com]"
+        exit
+fi
+
+curl "$1/robots.txt" > robots.txt
+
+firefox -net-tab `cat robots.txt | sed "s%Disallow: /%http://$1/%g" | grep http`
