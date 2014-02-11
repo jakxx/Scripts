af30cea5f631e3fa1e5359a3e486f5c74007816d
jakxx <jakx.ppr@gmail.com>
2014-02-11 10:39:50 -0500

    Added PowerShell local admin password function

---
+++ uniquepass.ps1
@@ -0,0 +1,36 @@
+function Hash($textToHash)
+<#  
+.SYNOPSIS  
+    PowerShell function to generate/retrieve unique local administrator passwords.        
+.DESCRIPTION  
+    Takes an MD5 hash of a (optional) unique string, a hardcoded string, and the hostname in which
+	the function is run on.
+.NOTES   
+    Author         : jakx_
+.EXAMPLE  
+    hash testing  
+#>
+{
+	$hostname = hostname
+
+	$s = "foobar"
+	
+	$new = "$hostname" + "$s" + "$texttoHash"
+	
+    $hashme = new-object System.Security.Cryptography.MD5CryptoServiceProvider
+
+    $tohash = [System.Text.Encoding]::UTF8.GetBytes($new)
+
+    $ByteArray = $hashme.ComputeHash($tohash)
+
+    foreach($byte in $ByteArray)
+
+    {
+
+         $pass += $byte.ToString()
+
+    }
+
+    return $pass;
+
+}
