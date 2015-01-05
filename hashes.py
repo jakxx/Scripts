49fbab515104700eabd755ccb6ac2d27afbd7982
jakxx <jakx.ppr@gmail.com>
2015-01-05 17:43:25 -0500

    Commit hashes.py

---
+++ hashes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import sys
+
+if len(str(sys.argv[1])) < 3:
+    print "usage: hash.py [hash]"
+    sys.exit()
+
+hash = str(sys.argv[1])
+with open("hashes.txt") as fd:
+    d = dict(line.strip().split(None, 1) for line in fd)
+
+print "---Specific Matches---"
+for x in d.keys():
+    if len(d[x]) == len(hash) and d[x][:1] is hash[:1] and d[x][1:2] == hash[1:2] and d[x][2:3] == hash[2:3]:
+        print str(x) + ":" + d[x]
+print "---Non-Specific Matches---"
+for x in d.keys():
+    if len(d[x]) == len(hash):
+        print str(x) + ":" + d[x]
+
