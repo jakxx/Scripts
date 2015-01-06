24cfa83af6bcfd4829f3c3c19c19ec646a0cce2a
jakxx <jakx.ppr@gmail.com>
2015-01-06 10:53:05 -0500

    Update

--- hashes.py
+++ hashes.py
@@ -9,11 +9,11 @@ hash = str(sys.argv[1])
 with open("hashes.txt") as fd:
     d = dict(line.strip().split(None, 1) for line in fd)
 
-print "---Specific Matches---"
+print "---Match Found---"
 for x in d.keys():
     if len(d[x]) == len(hash) and d[x][:1] is hash[:1] and d[x][1:2] == hash[1:2] and d[x][2:3] == hash[2:3]:
         print str(x) + ":" + d[x]
-print "---Non-Specific Matches---"
+print "---Other Possible Matches---"
 for x in d.keys():
     if len(d[x]) == len(hash):
         print str(x) + ":" + d[x]
