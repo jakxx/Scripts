8032c7c492aa13e58f65dc6e2ccb915504b7e744
jakxx <jakx.ppr@gmail.com>
2015-01-08 15:58:25 -0500

    new line

--- hashes.py
+++ hashes.py
@@ -13,7 +13,7 @@ print "---Match Found---"
 for x in d.keys():
     if len(d[x]) == len(hash) and d[x][:1] is hash[:1] and d[x][1:2] == hash[1:2] and d[x][2:3] == hash[2:3]:
         print str(x) + ":" + d[x]
-print "---Other Possible Matches---"
+print "\n---Other Possible Matches---"
 for x in d.keys():
     if len(d[x]) == len(hash):
         print str(x) + ":" + d[x]
