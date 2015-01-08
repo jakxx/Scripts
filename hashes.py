e14d40075e5bdf9e706fe6d8da69aa459c059ed9
jakxx <jakx.ppr@gmail.com>
2015-01-08 16:11:11 -0500

    wording

--- hashes.py
+++ hashes.py
@@ -10,7 +10,7 @@ with open("hashes.txt") as fd:
     d = dict(line.strip().split(None, 1) for line in fd)
 
 exact = ""
-print "---Match Found---"
+print "---Exact Match---"
 for x in d.keys():
     if len(d[x]) == len(hash) and d[x][:1] is hash[:1] and d[x][1:2] == hash[1:2] and d[x][2:3] == hash[2:3]:
         print str(x) + ":" + d[x]
