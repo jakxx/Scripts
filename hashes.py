005c1c38d8a45ad9fbdb62db3b0727262c5a6abb
jakxx <jakx.ppr@gmail.com>
2015-01-08 16:07:08 -0500

    exclude exact maches

--- hashes.py
+++ hashes.py
@@ -9,12 +9,14 @@ hash = str(sys.argv[1])
 with open("hashes.txt") as fd:
     d = dict(line.strip().split(None, 1) for line in fd)
 
+exact = ""
 print "---Match Found---"
 for x in d.keys():
     if len(d[x]) == len(hash) and d[x][:1] is hash[:1] and d[x][1:2] == hash[1:2] and d[x][2:3] == hash[2:3]:
         print str(x) + ":" + d[x]
+		exact = str(x)
 print "\n---Other Possible Matches---"
 for x in d.keys():
-    if len(d[x]) == len(hash):
+    if len(d[x]) == len(hash) and str(x) != exact:
         print str(x) + ":" + d[x]
 
