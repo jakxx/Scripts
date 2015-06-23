c3ae00d233e50b1c34903483519fa12406522344
jakxx <jakx.ppr@gmail.com>
2015-06-23 15:14:38 -0400

    Updates

--- robocop.py
+++ robocop.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python
 import urllib2
 import sys
+import webbrowser
 
-if len(str(sys.argv[1])) < 5 or sys.argv[1][0:4] != "http":
-    print"What are you doing? I need a real url..."
-    print "Usage: robocop.py http://www.google.com"
-    sys.exit()
-
+#Function to grab file and do stuff
 def grab(url):
     robots = urllib2.urlopen(url + "/robots.txt")
-    print line
+    data = robots.read()
+    data = data.split('\n')
+    entries = len(data)
+    resp = raw_input("Found " + str(entries) + " potential pages. About to open them in the browser. Cool? Y/N ")
+    webbrowser.get('firefox').open('http://www.google.com')
+    if resp == 'Y' or 'y':
+        for line in data:
+            if line.find('Disallow') != -1:
+                list = line.split(' ')
+                for url2 in list:
+                    if url2.find('Disallow') == -1:
+                        webbrowser.get('firefox').open_new_tab(url + url2)
+    else:
+        sys.exit()
 
-url = sys.argv[1]
-grab(url)
+#Function to parse argument
+def parse(argument):
+    try:
+        len(str(argument)) > 7 and argument[0:4] == "http"
+        return True
+    except:
+        print "What are you doing? I need a real url..."
+        print "Usage: robocop.py http://www.google.com"
+        sys.exit()
+
+if parse(sys.argv[1]):
+    grab(sys.argv[1])
