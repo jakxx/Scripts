81e9cbcd29fe4300bd3836983c7badc82654b8cc
jakxx <jakx.ppr@gmail.com>
2015-07-09 17:02:06 -0400

    Made target modular

--- quickie.py
+++ quickie.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
 import sys
 import urllib2
 
 file = sys.argv[1]
+target = sys.argv[2]
+
 f = open(file, "r")
 lines = f.readlines()
 for line in lines:
-    target = ".htaccess"
     line2 = line.strip()
     if line2[-1:] == '/':
         url = line2 + target
@@ -14,8 +14,8 @@ for line in lines:
         try:
             response = urllib2.urlopen(urlclean)
             html = response.read()
-            if html.find("doctype") == -1:
-                if response.code == 200:
+            if html.find("doctype") == -1:  #edit this for more specific results
+                if response.code == 200:    #edit this for mere specific results
                     print "Found at: " + urlclean
         except:
-            pass 
+            pass
