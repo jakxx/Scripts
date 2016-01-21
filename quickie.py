4047b9f17b8332998df286a04e4828674686e5b9
Andrew Smith <jakx.ppr@gmail.com>
2016-01-21 13:49:24 -0500

    Nead moar f00

--- quickie.py
+++ quickie.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 import sys
 import urllib2
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def usage():
    print " "
    print "Script to enumerate specific file from many urls"
-   print "USAGE: python quickie.py urls.txt \".htaccess\""
+   print "USAGE: python quickie.py urls.txt \"/.htaccess\""
    print " "
    sys.exit()
 
@@ -19,14 +23,18 @@ f = open(file, "r")
 lines = f.readlines()
 for line in lines:
     line2 = line.strip()
-    if line2[-1:] == '/':
+    if line2[-1:] == '/' or target[:1] == '/':
         url = line2 + target
         urlclean = url.replace ('\n', '')
         try:
-            response = urllib2.urlopen(urlclean)
-            html = response.read()
-            if html.find("doctype") == -1:  #edit this for more specific results
-                if response.code == 200:    #edit this for mere specific results
-                    print "Found at: " + urlclean
+            response = requests.get(urlclean,verify=False,timeout=1)
+            #html = response.read()
+            #if html.find("doctype") == -1:  #edit this for more specific results
+            if response.status_code == 200:    #edit this for mere specific results
+                redir = requests.get(urlclean + "/test.txt",verify=False,timeout=1)
+                if redir.status_code == 200:
+                   print "Found at: " + urlclean + " (likely a universal redirect)"
+                else:
+                   print "Found at: " + urlclean
         except:
             pass
