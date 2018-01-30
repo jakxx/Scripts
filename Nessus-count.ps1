4e09ea584adbb4ae6b4f5da6b78336bf426f065d
Andrew Smith <jakx.ppr@gmail.com>
2018-01-30 09:07:52 -0500

    Add Nessus Severity Count Script

---
+++ Nessus-count.ps1
@@ -0,0 +1,29 @@
+<#  
+.SYNOPSIS  
+    PowerShell function to count number of instances of critical, high, and medium severity nessus findings       
+.DESCRIPTION  
+    Takes a filepath for a Nessus CSV file
+.NOTES   
+    Author: jakx_
+.EXAMPLE  
+    count .\nessus.csv
+#>
+
+function count()
+{
+	Param (
+		[ValidateScript({Test-Path $_})]
+		[string]$csvfile
+	)
+	
+	#import csv
+	$impcsv = import-csv $csvfile
+	
+	#define risks we care about
+	$risks = @("Critical","High","Medium")
+	
+	foreach ($risk in $risks){
+		$count = $impcsv | select -unique risk, "plugin id" | where risk -eq $risk | measure-object | select -expandproperty count
+		write-host "$risk : $count"
+		}
+}
